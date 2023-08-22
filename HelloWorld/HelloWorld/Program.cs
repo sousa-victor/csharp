1c5edffa9abaf8e3b835dc9cc59761b5d9435c29
sousa-victor <90732816+sousa-victor@users.noreply.github.com>
2023-08-22 11:09:38 -0300

    Aula 01 - Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Oi!");
+            Console.WriteLine("Teste");
+        }
+    }
+}
